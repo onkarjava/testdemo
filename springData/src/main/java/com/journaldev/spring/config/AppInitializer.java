2d2ae9a676bdecdea7a37493a33d2c0037ba10d4
odeshpan <odeshpan@LIN80016470.corp.capgemini.com>
2019-01-15 09:56:44 +0530

    2nd commit

--- springData/src/main/java/com/journaldev/spring/config/AppInitializer.java
+++ springData/src/main/java/com/journaldev/spring/config/AppInitializer.java
@@ -14,4 +14,5 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+    //hello
 }
